2539daf20ee449ff7677071322ea563d029f9026
Ana Ribeiro <anaarsoares@gmail.com>
2024-06-19 16:07:46 +0100

    AI API integration

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
+function displayRecipe (response) {
+console.log("Recipe generated");
+
+    new Typewriter('.recipe-instructions', {
+        strings: response.data.answer,
+        autoStart: true,
+        delay: 10,
+        cursor: "",
+      });
+
+}
+
 function generateRecipe (event) {
     event.preventDefault();
 
+    console.log("Generating recipe");
 
-    new Typewriter('.recipe-instructions', {
-        strings: "Generating a recipe for you!",
-        autoStart: true,
-        delay: 1,
-        cursor: "",
-      });
+    let ingredientsEntered = document.querySelector(".ingredients");
+
+    let apiKey = "279o37234fc0ba3effba9a107a3601tc";
+    let context = "You are a great cook, could you please generate an easy to follow recipe with the following ingredients, using the metric system for ingredients measurements and celsius degrees for temperatures."
+    let prompt = `Please generate a recipe with this ingredients ${ingredientsEntered.value} the ingredients needed must be showned in a <strong> HTML element, and each step of the recipe must be separated by a <br> HTML element`;
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    
+    axios.get(apiUrl).then(displayRecipe)
+    
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator");
 recipeFormElement.addEventListener("submit", generateRecipe);
+
+ 
